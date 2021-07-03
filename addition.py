87d8df0c89233fc28e422fa773bd39f0a5d2d7af
Rustin Soul <rustin@localhost.localdomain>
2021-07-03 18:56:22 +0530

    normal addition of numbers(0-100) using for and while  loop

---
+++ addition.py
@@ -0,0 +1,16 @@
+#normal addition of numbers from 0 to 100 using for and while loop
+
+#for for loop
+
+'''total= 0
+for num in range(0,101):
+	total= total+num
+print(total)'''
+
+#for while loop
+total =0
+num= 0
+while num in range(0,101):
+	total= total + num
+	num= num + 1
+print(total)
